6b9b983a4fddbeeeae897c3d3552a532476011c7
silvia.rcr <silvia.rcr@gmail.com>
2018-01-18 16:00:27 -0200

    aula 4 concluida

--- public/js/main.js
+++ public/js/main.js
@@ -1,49 +1,64 @@
-// var frase = jQuery(".frase");  
-//O atalho para a função jQuery é representado pelo símbolo $.
-
-var frase = $(".frase").text();
-
-var numPalavras = frase.split(" ").length;
-var tamanhoFrase = $("#tamanho-frase");
-tamanhoFrase.text(numPalavras);
-
+var tempoInicial = $("#tempo-digitacao").text();
 var campo = $(".campo-digitacao");
-// campo.on("click", function() {  //atualiza contador quando usuário clica
-campo.on("input", function () {     //atualiza contador enquanto o usuário digita
-    var conteudo = campo.val();
 
-    // var qtdPalavras = conteudo.split(" ").length; //se apagarmos uma frase, ainda se conta uma 
-    //e se dermos vários espaços, conta como se cada espaço fosse uma palavra
-
-    var qtdPalavras = conteudo.split(/\S+/).length - 1; //Regexp -> expressão regular que busca qualquer caractere, 
-    //exceto espaço vazio  /\S+/ e a contagem sempre mostra
-    // a quantidade de palavras mais uma -> subtrair 1
-    $("#contador-palavras").text(qtdPalavras);
-
-    var qtdCaracteres = conteudo.length;
-    $("#contador-caracteres").text(qtdCaracteres);
-
+// $(document).ready(function() substituida pelo atalho $(function() {....})
+    $(function() {
+    atualizaTamanhoFrase();
+    inicializaContadores();
+    inicializaCronometro();
+    $("#botao-reiniciar").click(reiniciaJogo);
 });
 
-var tempoRestante = $("#tempo-digitacao").text();
-campo.one("focus", function () {
-    var cronometroID = setInterval(function () {
-        tempoRestante--;
-        $("#tempo-digitacao").text(tempoRestante);
+//bloco de código responsável por atualizar o tamanho da frase, vamos envolver numa função de mesmo nome
+function atualizaTamanhoFrase() {
+    var frase = $(".frase").text();
+    var numPalavras = frase.split(" ").length;
+    var tamanhoFrase = $("#tamanho-frase");
+    tamanhoFrase.text(numPalavras);
+}
 
-        if (tempoRestante < 1) {
-            campo.attr("disabled", true);
-            clearInterval(cronometroID);
-        }
+//inicializa contadores
+function inicializaContadores() {
 
-    }, 1000);
-});
+    campo.on("input", function () {
+        var conteudo = campo.val();
 
-$("#botao-reiniciar").click(function() {
-    console.log("reiniciando o jogo");
+        var qtdPalavras = conteudo.split(/\S+/).length - 1;
+        $("#contador-palavras").text(qtdPalavras);
+
+        var qtdCaracteres = conteudo.length;
+        $("#contador-caracteres").text(qtdCaracteres);
+
+    });
+}
+
+//inicializa o cronometro para marcar o tempo
+
+function inicializaCronometro() {
+
+    var tempoRestante = $("#tempo-digitacao").text();
+    campo.one("focus", function () {
+        $("#botao-reiniciar").attr("disabled",true);
+        var cronometroID = setInterval(function () {
+            tempoRestante--;
+            $("#tempo-digitacao").text(tempoRestante);
+
+            if (tempoRestante < 1) {
+                campo.attr("disabled", true);
+                clearInterval(cronometroID);
+                $("#botao-reiniciar").attr("disabled",false);
+            }
+
+        }, 1000);
+    });
+}
+
+//reiniciar jogo
+function reiniciaJogo() {
     campo.attr("disabled", false);
     campo.val("");
     $("#contador-caracteres").text("0");
     $("#contador-palavras").text("0");
-    console.log(contador-palavras)
-});
+    $("#tempo-digitacao").text(tempoInicial);
+    inicializaCronometro();
+}
